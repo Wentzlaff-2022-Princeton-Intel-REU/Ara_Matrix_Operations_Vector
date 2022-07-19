f82fa296853925f7a5743110373b0853ccf34bca
Manya Zhu <manya.zhu@gmail.com>
2022-07-19 16:12:24 -0400

    all files

---
+++ multiply.h
@@ -0,0 +1,11 @@
+#pragma once
+/*------------------------------------------------------------*/
+/* multiply.h                                                 */
+/*------------------------------------------------------------*/
+
+#include "matrix.h"
+
+/*------------------------------------------------------------*/
+
+// Multiplies two matrices with vector procesing
+void multiply(Matrix_t mat1, Matrix_t mat2, Matrix_t* resultingMatrix);
---
+++ print.h
@@ -0,0 +1,11 @@
+#pragma once
+/*------------------------------------------------------------*/
+/* print.c                                                    */
+/*------------------------------------------------------------*/
+
+#include "matrix.h"
+#includ "printf.h"          
+
+/*------------------------------------------------------------*/
+// prints out the elements in the given matrix
+void printMatrix(Matrix_t mat);
