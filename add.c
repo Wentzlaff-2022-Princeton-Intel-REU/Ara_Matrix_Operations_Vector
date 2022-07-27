43d29a05d5dfdae86ddb6c4fbc38f4a214bb1f82
m-ukund <mramakri@outlook.com>
2022-07-27 16:56:12 -0400

    vMat1

--- add.c
+++ add.c
@@ -24,7 +24,7 @@ void add(Matrix_t mat1, Matrix_t mat2, Matrix_t *resultingMatrix) {
     for (size_t vl; (vl = vsetvl_e32m1(avl)) > 0; avl -= vl) {
         vMat1 = vle32_v_i32m1(addend1, vl);
         vMat2 = vle32_v_i32m1(addend2, vl);
-        vSum = vMat1dd_vv_i32m1(va, vMat2, vl);
+        vSum = vMat1dd_vv_i32m1(vMat1, vMat2, vl);
         vse32_v_i32m1(sum, vSum, vl);
         addend1 += vl;
         addend2 += vl;
